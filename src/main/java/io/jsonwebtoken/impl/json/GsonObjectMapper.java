7268eda4179bdf864dd67bfdee531434afa66bc2
Craig Day <craigday@gmail.com>
2016-05-12 20:43:25 +0800

    Cleanup licenses and javadoc

--- src/main/java/io/jsonwebtoken/impl/json/GsonObjectMapper.java
+++ src/main/java/io/jsonwebtoken/impl/json/GsonObjectMapper.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 jsonwebtoken.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jsonwebtoken.impl.json;
 
 import com.google.gson.Gson;
@@ -10,23 +25,18 @@ import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 import com.google.gson.JsonSyntaxException;
-import com.google.gson.LongSerializationPolicy;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.util.Date;
 
-/**
- * @author Craig Day (craig@livewirelabs.com.au)
- */
 public class GsonObjectMapper implements ObjectMapper {
 
     private final Gson gson;
 
     public GsonObjectMapper() {
         GsonBuilder builder = new GsonBuilder();
-        builder.setLongSerializationPolicy(LongSerializationPolicy.STRING);
         builder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
             @Override
             public Date deserialize(JsonElement json,
@@ -59,10 +69,4 @@ public class GsonObjectMapper implements ObjectMapper {
             throw new IOException(e);
         }
     }
-
-
-    public static void main(String[] args) {
-        Gson gson = new Gson();
-    }
-
 }
